d57404074ac43d3e9b1cd0a92b10786be1eb8ce2
海绵大叔 <65875847+haimian467@users.noreply.github.com>
2023-07-20 09:00:54 +0800

    Delete 工控论坛刷单个文章阅读.py

--- 工控论坛刷单个文章阅读.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-
-url="http://www.ymmfa.com/read-gktid-1749887.html"
-req=urllib.request.Request(url)
-resp=urllib.request.urlopen(req)
-data=resp.read().decode('gbk')
-
-print(data)
